3fa221d58630107d2ae48084dc4b181270149265
lazizbek97 <laziz.fayziev@mail.ru>
2022-07-12 18:18:10 +0500

    new release

--- lib/main.dart
+++ lib/main.dart
@@ -157,7 +157,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.cyanAccent,
+        backgroundColor: Colors.greenAccent,
         title: Text(
           widget.title,
           style: const TextStyle(color: Colors.black),
